8b8202b3684bc873fdf0c014adfb579432b5389e
Keegan Ravindran <keegan.r.personal@gmail.com>
2025-04-17 00:01:10 +0800

    new seeds

--- run-multicore.bat
+++ run-multicore.bat
@@ -7,7 +7,7 @@ set IMAGE=libmspack-fuzz
 
 echo [+] Starting AFL++ master and workers with ASAN and auto-resume...
 
-REM Master instance
+REM Master instance (explore strategy)
 start "fuzzer-master" docker run -it ^
   -e AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 ^
   -e AFL_AUTORESUME=1 ^
@@ -15,9 +15,9 @@ start "fuzzer-master" docker run -it ^
   -e ASAN_OPTIONS=detect_leaks=0:symbolize=0:abort_on_error=1 ^
   -v "%SEEDS_DIR%":/seeds ^
   -v "%OUT_DIR%":/findings ^
-  %IMAGE% bash -c "afl-fuzz -M fuzzer-master -i /seeds -o /findings -- ./fuzz_cab_open @@"
+  %IMAGE% bash -c "exec afl-fuzz -M fuzzer-master -i /seeds -o /findings -p explore -- ./fuzz_cab_open @@"
 
-REM Worker instance 1
+REM Worker instance 1 (fast strategy)
 start "fuzzer-worker1" docker run -it --rm ^
   -e AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 ^
   -e AFL_AUTORESUME=1 ^
@@ -25,9 +25,9 @@ start "fuzzer-worker1" docker run -it --rm ^
   -e ASAN_OPTIONS=detect_leaks=0:symbolize=0:abort_on_error=1 ^
   -v "%SEEDS_DIR%":/seeds ^
   -v "%OUT_DIR%":/findings ^
-  %IMAGE% bash -c "afl-fuzz -S fuzzer-worker1 -i /seeds -o /findings -- ./fuzz_cab_open @@"
+  %IMAGE% bash -c "exec afl-fuzz -S fuzzer-worker1 -i /seeds -o /findings -p fast -- ./fuzz_cab_open @@"
 
-REM Worker instance 2
+REM Worker instance 2 (exploit strategy)
 start "fuzzer-worker2" docker run -it --rm ^
   -e AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 ^
   -e AFL_AUTORESUME=1 ^
@@ -35,6 +35,6 @@ start "fuzzer-worker2" docker run -it --rm ^
   -e ASAN_OPTIONS=detect_leaks=0:symbolize=0:abort_on_error=1 ^
   -v "%SEEDS_DIR%":/seeds ^
   -v "%OUT_DIR%":/findings ^
-  %IMAGE% bash -c "afl-fuzz -S fuzzer-worker2 -i /seeds -o /findings -- ./fuzz_cab_open @@"
+  %IMAGE% bash -c "exec afl-fuzz -S fuzzer-worker2 -i /seeds -o /findings -p exploit -- ./fuzz_cab_open @@"
 
 endlocal
